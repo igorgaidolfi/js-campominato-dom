14dfd063943db255bb729f7c7812599cfffbad76
igorgaidolfi <gandalf.4@outlook.it>
2023-11-30 16:30:49 +0100

    update Edited script checkUnique fun()

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
+function checkUniqueBomb(array_bomb){
+    let flag = true
+    while(flag){
+        let bomb = Math.floor(Math.random()*100)+1
+        if(!array_bomb.include(bomb)){
+            flag = false
+        }
+    }
+    return bomb
+}
+// function bombMaker(){
+//     let array_bomb = []
+//     for(let i=0;i<16;i++){
+//         array_bomb[i] = ciao
+//     }
+//     return array_bomb
+// }
+// function fillBomb(){
+
+// }
 function cellMaker(num){
     const element = document.createElement('div')
     element.classList.add('square')
     element.innerText = num
     return element
 }
+
 const grid = document.getElementById('grid')
 const button = document.getElementById('play')
 button.addEventListener("click", function(){
@@ -15,3 +36,8 @@ button.addEventListener("click", function(){
         grid.appendChild(square)
     }
 })
+let pio=[]
+for(let i=0;i<5;i++){
+    pio[i] = Math.floor(Math.random()*100)+1
+}
+console.log(pio)
